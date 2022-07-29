bc14715199944d64c19c24982947142077f52fa9
Alex Acebo <44811138+aacebo@users.noreply.github.com>
2022-07-29 16:34:12 -0400

    Initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/aacebo/owl
+
+go 1.18
---
+++ string.go
@@ -0,0 +1,15 @@
+package owl
+
+type StringSchema struct {
+	ops: []*interface{test: string}
+}
+
+func String() *StringSchema {
+	v := StringSchema{}
+
+	return &v
+}
+
+func (self *StringSchema) Min(v int) *StringSchema {
+
+}
