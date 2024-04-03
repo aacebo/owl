aae2bf318e7888bd9429a54f0d748da490068d88
aacebo <aacebowork@gmail.com>
2024-04-03 19:20:03 -0400

    Update pattern_test.go

--- pattern_test.go
+++ pattern_test.go
@@ -27,11 +27,21 @@ func Test_Pattern(t *testing.T) {
 		}
 	})
 
-	t.Run("should succeed", func(t *testing.T) {
+	t.Run("should succeed on numeric", func(t *testing.T) {
 		str := "123"
 		errs := owl.Validate(struct {
 			A *string `json:"a" owl:"pattern=^[0-9]*$"`
 		}{&str})
+
+		if len(errs) > 0 {
+			t.Error(errs)
+		}
+	})
+
+	t.Run("should succeed on empty", func(t *testing.T) {
+		errs := owl.Validate(struct {
+			A string `json:"a" owl:"pattern="`
+		}{"123"})
 
 		if len(errs) > 0 {
 			t.Error(errs)
