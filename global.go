291cd9449b8fa75927942d4365d0474e0c8a17d0
aacebo <aacebowork@gmail.com>
2024-04-03 20:34:33 -0400

    Update global.go

--- global.go
+++ global.go
@@ -6,6 +6,10 @@ func AddRule(name string, rule Rule) {
 	validator.AddRule(name, rule)
 }
 
+func AddFormat(name string, formatter Formatter) {
+	validator.AddFormat(name, formatter)
+}
+
 func Validate(v any) []Error {
 	return validator.Validate(v)
 }
