a2ef6f7f11be82e813dbefbd5942afe86e09f6e1
aacebo <aacebowork@gmail.com>
2024-04-06 02:17:53 -0400

    Create validator_test.go

---
+++ validator_test.go
@@ -0,0 +1,35 @@
+package owl_test
+
+import (
+	"testing"
+
+	"github.com/aacebo/owl"
+)
+
+type Address struct {
+	Street string `json:"street" owl:"required"`
+	City   string `json:"city" owl:"required"`
+	State  string `json:"state" owl:"required"`
+	Zip    int    `json:"zip" owl:"required,min=00501,max=89049"`
+}
+
+func Test_Validator(t *testing.T) {
+	t.Run("should succeed with nested complex structure", func(t *testing.T) {
+		errs := owl.Validate(struct {
+			Name    string  `json:"name" owl:"required,min=3"`
+			Address Address `json:"address"`
+		}{
+			Name: "hii",
+			Address: Address{
+				Street: "111 My Street Way",
+				City:   "New York",
+				State:  "New York",
+				Zip:    11249,
+			},
+		})
+
+		if len(errs) > 0 {
+			t.Error(errs)
+		}
+	})
+}
